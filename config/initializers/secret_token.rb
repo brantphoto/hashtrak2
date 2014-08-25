1ac9017ff2213c0b2d0befe0bd6a694c93387c81
brantphoto <brant.barger@gmail.com>
2014-08-25 10:25:26 -0700

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Hashtrek::Application.config.secret_key_base = 'f2fe30f4589e770dbdc010a18d1bef3a71b5482adf04046d592bfc45bc7c289c327bfc71987beaf694832b8e314dc46ef824b27b4a6ff0e73fc9171fabc58b0c'
