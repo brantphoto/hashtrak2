c29b89ad7bd5fe25ca1add828a769746a65da58a
lmaisour <lmaisour@gmail.com>
2014-08-27 11:24:24 -0700

    added post resources

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,9 @@ Hashtrek::Application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  root 'pages#home'
+  root 'posts#index'
+
+  resources :posts
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
