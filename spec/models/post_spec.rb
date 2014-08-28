d5ea5503209fae63c434d7e475cc87e88d182169
lmaisour <lmaisour@gmail.com>
2014-08-28 11:41:16 -0700

    started testing for Post model

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -1,5 +1,14 @@
 require 'rails_helper'
 
 RSpec.describe Post, :type => :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+ before { @post = Post.create!(insta_user:InstaUser.first)}
+ subject { @post }
+
+# describe Post do
+	it { should have_many(:related_hashtag_posts) }
+	it { should have_many(:related_hashtags) }
+	it { should belong_to(:insta_user) }
+	it { should belong_to(:hashtag_feed) }
 end
+
+
