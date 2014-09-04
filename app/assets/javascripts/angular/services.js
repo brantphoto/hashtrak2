3f910f5cad03a74c62ceb98348dc6e4c4b5a22d6
lmaisour <lmaisour@gmail.com>
2014-09-04 16:01:05 -0700

    still not working

--- app/assets/javascripts/angular/services.js
+++ app/assets/javascripts/angular/services.js
@@ -1,5 +1,5 @@
 relatedManager.factory('RelatedHashtag', ['$resource', function($resource){
-	return $resource('/hashtag_feeds/5/related_hashtags.json',
+	return $resource('/hashtag_feeds/3/related_hashtags.json',
 		{id: '@id'},
 		{update: {method: 'PATCH'}});
 }]);
