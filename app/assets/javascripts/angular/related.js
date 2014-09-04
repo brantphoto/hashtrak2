d3efe3ebf220998a12ded3b37cc55a4b704abade
brantphoto <brant.barger@gmail.com>
2014-09-03 21:56:10 -0700

    angular working. Look at the varibles on the related.js and the related_hashtag view

--- app/assets/javascripts/angular/related.js
+++ app/assets/javascripts/angular/related.js
@@ -3,8 +3,22 @@ relatedManager.controller('RelatedCtrl', ['$scope', 'RelatedHashtag', function($
 	this.test = 'fuck yeah angular';
 
 	RelatedHashtag.query(function(json){
-      $scope.tags = json;
-      console.log(json);
+      tags = json;
+      $scope.tags = tags;
+      $scope.x1 = tags[0][0]
+      $scope.x2 = tags[1][0]
+      $scope.x3 = tags[2][0]
+      $scope.x4 = tags[3][0]
+      $scope.x5 = tags[4][0]
+      $scope.y1 = tags[0][1]
+      $scope.y2 = tags[1][1]
+      $scope.y3 = tags[2][1]
+      $scope.y4 = tags[3][1]
+      $scope.y5 = tags[4][1]
+
+      // $scope. = tags[0];
+
+
 	})
 
 
