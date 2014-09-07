325971dc6c8393c0abab06d070626a1ce8b5f741
lmaisour <lmaisour@gmail.com>
2014-09-06 18:33:32 -0700

    Added line plot and bar chart to view. still not able to display data dynamically

--- app/assets/javascripts/angular/related.js
+++ app/assets/javascripts/angular/related.js
@@ -1,8 +1,9 @@
 relatedManager.controller('RelatedCtrl', ['$scope', 'RelatedHashtag',  function($scope, RelatedHashtag){
 
-	this.test = 'fuck yeah angular';
+    this.test = 'fuck yeah angular';
 
-      RelatedHashtag.query(function(json){
+
+  RelatedHashtag.query(function(json){
       tags = json;
       $scope.tags = tags;
       $scope.x1 = tags[0][0]
@@ -15,34 +16,184 @@ relatedManager.controller('RelatedCtrl', ['$scope', 'RelatedHashtag',  function(
       $scope.y3 = tags[2][1]
       $scope.y4 = tags[3][1]
       $scope.y5 = tags[4][1]
-      })
 
-
-        $scope.myChartData = [
-        {
-            value: 15 ,
-            color:"#F7464A"
+      $scope.myChartData = 
+      [
+          {
+            value: 30,
+            color:"#F7464A  "
         },
         {
             value : 50,
-            color : "#E2EAE9"
+            color : "#E2EAE9  "
         },
         {
             value : 100,
-            color : "#D4CCC5"
+            color : "#D4CCC5  "
         },
         {
             value : 40,
-            color : "#949FB1"
+            color : "#949FB1  "
         },
         {
             value : 120,
-            color : "#4D5360"
+            color : "#4D5360  "
         }
-    ];
+    ]
+
+    $scope.chart = {
+        labels : ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday"],
+        datasets : [
+        {
+            fillColor : "rgba(151,187,205,0)",
+            strokeColor : "#e67e22  ",
+            pointColor : "rgba(151,187,205,0)",
+            pointStrokeColor : "#e67e22  ",
+            data : [4, 3, 5, 4, 6]
+        },
+
+        ], 
+    };
+
+    $scope.line = {
+    labels: ["January", "February", "March", "April", "May", "June", "July"],
+    datasets: [
+        {
+            label: "My First dataset",
+            fillColor: "rgba(220,220,220,0.2)",
+            strokeColor: "rgba(220,220,220,1)",
+            pointColor: "rgba(220,220,220,1)",
+            pointStrokeColor: "#fff",
+            pointHighlightFill: "#fff",
+            pointHighlightStroke: "rgba(220,220,220,1)",
+            data: [65, 59, 80, 81, 56, 55, 40]
+        },
+    ]
+};
+
+
+});
 
     //Globals
     $scope.myChartOptions = {
+        // Boolean - Whether to animate the chart
+        animation: true,
+
+        // Number - Number of animation steps
+        animationSteps: 60,
+
+        // String - Animation easing effect
+        animationEasing: "easeOutQuart",
+
+        // Boolean - If we should show the scale at all
+        showScale: true,
+
+        // Boolean - If we want to override with a hard coded scale
+        scaleOverride: false,
+
+        // ** Required if scaleOverride is true **
+        // Number - The number of steps in a hard coded scale
+        scaleSteps: null,
+        // Number - The value jump in the hard coded scale
+        scaleStepWidth: null,
+        // Number - The scale starting value
+        scaleStartValue: null,
+
+        // String - Colour of the scale line
+        scaleLineColor: "rgba(0,0,0,.1)",
+
+        // Number - Pixel width of the scale line
+        scaleLineWidth: 1,
+
+        // Boolean - Whether to show labels on the scale
+        scaleShowLabels: true,
+
+        // Interpolated JS string - can access value
+        scaleLabel: "<%=value%>",
+
+        // Boolean - Whether the scale should stick to integers, not floats even if drawing space is there
+        scaleIntegersOnly: true,
+
+        // Boolean - Whether the scale should start at zero, or an order of magnitude down from the lowest value
+        scaleBeginAtZero: false,
+
+        // String - Scale label font declaration for the scale label
+        scaleFontFamily: "'Helvetica Neue', 'Helvetica', 'Arial', sans-serif",
+
+        // Number - Scale label font size in pixels
+        scaleFontSize: 12,
+
+        // String - Scale label font weight style
+        scaleFontStyle: "normal",
+
+        // String - Scale label font colour
+        scaleFontColor: "#666",
+
+        // Boolean - whether or not the chart should be responsive and resize when the browser does.
+        responsive: false,
+
+        // Boolean - Determines whether to draw tooltips on the canvas or not
+        showTooltips: true,
+
+        // Array - Array of string names to attach tooltip events
+        tooltipEvents: ["mousemove", "touchstart", "touchmove"],
+
+        // String - Tooltip background colour
+        tooltipFillColor: "rgba(0,0,0,0.8)",
+
+        // String - Tooltip label font declaration for the scale label
+        tooltipFontFamily: "'Helvetica Neue', 'Helvetica', 'Arial', sans-serif",
+
+        // Number - Tooltip label font size in pixels
+        tooltipFontSize: 14,
+
+        // String - Tooltip font weight style
+        tooltipFontStyle: "normal",
+
+        // String - Tooltip label font colour
+        tooltipFontColor: "#fff",
+
+        // String - Tooltip title font declaration for the scale label
+        tooltipTitleFontFamily: "'Helvetica Neue', 'Helvetica', 'Arial', sans-serif",
+
+        // Number - Tooltip title font size in pixels
+        tooltipTitleFontSize: 14,
+
+        // String - Tooltip title font weight style
+        tooltipTitleFontStyle: "bold",
+
+        // String - Tooltip title font colour
+        tooltipTitleFontColor: "#fff",
+
+        // Number - pixel width of padding around tooltip text
+        tooltipYPadding: 6,
+
+        // Number - pixel width of padding around tooltip text
+        tooltipXPadding: 6,
+
+        // Number - Size of the caret on the tooltip
+        tooltipCaretSize: 8,
+
+        // Number - Pixel radius of the tooltip border
+        tooltipCornerRadius: 6,
+
+        // Number - Pixel offset from point x to tooltip edge
+        tooltipXOffset: 10,
+
+        // String - Template string for single tooltips
+        tooltipTemplate: "<%if (label){%><%=label%>: <%}%><%= value %>",
+
+        // String - Template string for single tooltips
+        multiTooltipTemplate: "<%= value %>",
+
+        // Function - Will fire on animation progression.
+        onAnimationProgress: function(){},
+
+        // Function - Will fire on animation completion.
+        onAnimationComplete: function(){}
+    };
+
+    $scope.myLineOptions = {
         // Boolean - Whether to animate the chart
         animation: true,
 
@@ -164,4 +315,4 @@ relatedManager.controller('RelatedCtrl', ['$scope', 'RelatedHashtag',  function(
 
 
 
-	}]);
+}]);
