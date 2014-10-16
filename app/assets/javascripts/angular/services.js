a260a84ed2dbc78908b7899f2c7a5066fa93cc07
brantphoto <brant.barger@gmail.com>
2014-10-16 16:11:46 -0700

    fixed related.js and services.js to properly accept the api json. Now working

--- app/assets/javascripts/angular/services.js
+++ app/assets/javascripts/angular/services.js
@@ -1,6 +1,6 @@
 relatedManager.factory('HashtagFeed', ['$resource', function($resource){
 
-	return $resource('/hashtag_feeds/:id',
+	return $resource('/hashtag_feeds/:id.json',
 		{ id: '@id' },
 		{update: {method: 'PATCH'} }
 		);
