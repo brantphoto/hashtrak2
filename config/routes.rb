340c071611231f4eecdba8b840fb886e1f0df975
brantphoto <brant.barger@gmail.com>
2014-10-22 10:00:35 -0700

    deleted searches#new view and changed root to hashtag_feeds#index

--- config/routes.rb
+++ config/routes.rb
@@ -1,27 +1,4 @@
 Hashtrek::Application.routes.draw do
-  #get "hash_sizes/index"
-  #get "categories/index"
-  #get "categories/show"
-  #get "searches/index"
-  #get "searches/new"
-  # get "searches/create"
-  # get "searches/destroy"
-  # get "related_hashtags/index"
-  # get "related_hashtags/new"
-  # get "related_hashtags/create"
-  # get "related_hashtags/show"
-  # get "related_hashtags/update"
-  # get "related_hashtags/destroy"
-  # get "hashtag_feeds/index"
-  # get "hashtag_feeds/new"
-  # get "hashtag_feeds/create"
-  # get "hashtag_feeds/edit"
-  # get "hashtag_feeds/update"
-  # get "hashtag_feeds/destroy"
-  # The priority is based upon order of creation: first created -> highest priority.
-  # See how all your routes lay out with "rake routes".
-
-  # You can have the root of your site routed with "root"
 
   resources :categories
   resources :searches
@@ -29,56 +6,5 @@ Hashtrek::Application.routes.draw do
     resources :hash_sizes, only: [:index, :show], defaults: { format: :json }
   end
 
-  root 'searches#new'
-# scope '/api' do
-#   resources :hashtag_feeds, only: [:index, :show], defaults: { format: :json }
-# end
-  # Example of regular route:
-  #   get 'products/:id' => 'catalog#view'
-
-  # Example of named route that can be invoked with purchase_url(id: product.id)
-  #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
-
-  # Example resource route (maps HTTP verbs to controller actions automatically):
-  #   resources :products
-
-  # Example resource route with options:
-  #   resources :products do
-  #     member do
-  #       get 'short'
-  #       post 'toggle'
-  #     end
-  #
-  #     collection do
-  #       get 'sold'
-  #     end
-  #   end
-
-  # Example resource route with sub-resources:
-  #   resources :products do
-  #     resources :comments, :sales
-  #     resource :seller
-  #   end
-
-  # Example resource route with more complex sub-resources:
-  #   resources :products do
-  #     resources :comments
-  #     resources :sales do
-  #       get 'recent', on: :collection
-  #     end
-  #   end
-
-  # Example resource route with concerns:
-  #   concern :toggleable do
-  #     post 'toggle'
-  #   end
-  #   resources :posts, concerns: :toggleable
-  #   resources :photos, concerns: :toggleable
-
-  # Example resource route within a namespace:
-  #   namespace :admin do
-  #     # Directs /admin/products/* to Admin::ProductsController
-  #     # (app/controllers/admin/products_controller.rb)
-  #     resources :products
-  #   end
+  root 'hashtag_feeds#index'
 end
